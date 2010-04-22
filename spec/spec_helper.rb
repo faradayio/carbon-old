a1492f758b71437f648505808fca00bc9198a655
Derek Kastner <dkastner@gmail.com>
2010-04-22 17:23:25 -0400

    added smarts to load shared example groups

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,7 @@ carbon_path = File.expand_path(File.dirname(__FILE__) + "/../lib/")
 $LOAD_PATH.unshift(carbon_path) unless $LOAD_PATH.include?(carbon_path)
 
 require "carbon"
+
+Dir.glob(File.expand_path(File.dirname(__FILE__) + '/lib/**/*_examples.rb')).each do |file|
+  require file
+end
