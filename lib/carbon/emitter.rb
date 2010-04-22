c643ca095670899b23fc74b9e544a981f6d40e82
Derek Kastner <dkastner@gmail.com>
2010-04-22 17:23:43 -0400

    generic emitter class

--- lib/carbon/emitter.rb
+++ lib/carbon/emitter.rb
@@ -1,7 +1,18 @@
 module Carbon
   module Emitter
-    def calculate
+    class NotYetCalculated < StandardError; end
 
+    attr_accessor :methodology_url
+    attr_reader :footprint
+    
+    def methodology_url
+      raise Emitter::NotYetCalculated if footprint.nil?
+      @methodology_url
+    end
+
+    def calculate!
+      @footprint = 0
+      @methodology_url = 'http://carbon.brighterplanet.com'
     end
   end
 end
