35b4327a8e7dde095264d403462b9773960f3e00
Derek Kastner <dkastner@gmail.com>
2010-04-23 15:10:56 -0400

    finished basic Flight emitter

--- lib/carbon/flight.rb
+++ lib/carbon/flight.rb
@@ -1,6 +1,10 @@
 module Carbon
   class Flight
-    include Emitter
+    include Carbon::Emitter
 
+    characteristics :date, :year, :time_of_day, :destination_airport, 
+      :origin_airport, :distance_class, :distance_estimate, :domesticity, 
+      :airline, :trips, :emplanements_per_trip, :seat_class, :load_factor, 
+      :seats_estimate, :aircraft_class, :aircraft, :propulsion
   end
 end
