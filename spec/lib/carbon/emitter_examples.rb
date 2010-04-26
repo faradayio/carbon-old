217ca8388310f2ac2f9f7ba752a12edd330238f7
Derek Kastner <dkastner@gmail.com>
2010-04-26 11:28:25 -0400

    got rid of non-DSL API

--- spec/lib/carbon/emitter_examples.rb
+++
@@ -1,9 +0,0 @@
-shared_examples_for Carbon::Emitter.to_s do
-  it { should be_a_kind_of(Carbon::Emitter) }
-
-  let(:resource_object) { emitter }
-  it_should_behave_like Carbon::Resource.to_s
-
-  it { should respond_to(:emission) }
-  it { should respond_to(:methodology) }
-end
--- spec/lib/carbon/resource_examples.rb
+++
@@ -1,5 +0,0 @@
-require 'rest_client'
-
-shared_examples_for Carbon::Resource.to_s do
-  it { should be_a_kind_of(Carbon::Resource) }
-end
