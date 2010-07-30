bde143bdee48c71c90ebd63046f03a7baf1a1f36
Seamus Abshere <seamus@abshere.net>
2010-07-30 10:53:35 -0400

    ::Carbon::Storage -> ::Carbon::EmissionEstimate::Storage

--- lib/carbon/emission_estimate/storage.rb
+++ lib/carbon/emission_estimate/storage.rb
@@ -1,27 +1,29 @@
 module Carbon
-  class Storage
-    attr_accessor :parent
-    attr_reader :raw_request
-    attr_reader :raw_response
-    def initialize(parent)
-      @parent = parent
-    end
-    def url
-      "#{::Carbon::STORAGE_URL}/#{::Digest::SHA1.hexdigest(parent.key+parent.guid)}"
-    end
-    def present?
-      parent.guid.present? and data.present?
-    end
-    def data
-      return @data[0] if @data.is_a? ::Array
-      @raw_request = ::REST::Request.new :get, ::URI.parse(url)
-      @raw_response = raw_request.perform
-      if raw_response.success?
-        @data = [::Carbon::EmissionEstimate.parse(raw_response.body)]
-      else
-        @data = []
+  class EmissionEstimate
+    class Storage
+      attr_accessor :parent
+      attr_reader :raw_request
+      attr_reader :raw_response
+      def initialize(parent)
+        @parent = parent
       end
-      @data[0]
+      def url
+        "#{::Carbon::STORAGE_URL}/#{::Digest::SHA1.hexdigest(parent.key+parent.guid)}"
+      end
+      def present?
+        parent.guid.present? and data.present?
+      end
+      def data
+        return @data[0] if @data.is_a? ::Array
+        @raw_request = ::REST::Request.new :get, ::URI.parse(url)
+        @raw_response = raw_request.perform
+        if raw_response.success?
+          @data = [::Carbon::EmissionEstimate.parse(raw_response.body)]
+        else
+          @data = []
+        end
+        @data[0]
+      end
     end
   end
 end
